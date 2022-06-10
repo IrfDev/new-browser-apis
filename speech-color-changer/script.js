fcc2d373f7afb2e75093baf4e02fbd0dbb42ffc7
Irving <irving.dev@icloud.com>
2022-06-09 22:51:50 -0500

    Improvement: Adding tailwind

--- speech-color-changer/script.js
+++ speech-color-changer/script.js
@@ -1,62 +1,152 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
-var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 
-var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList;
+
+var SpeechRecognitionEvent =
+  SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+var colors = [
+  "aqua",
+  "azure",
+  "beige",
+  "bisque",
+  "black",
+  "blue",
+  "brown",
+  "chocolate",
+  "coral",
+  "crimson",
+  "cyan",
+  "fuchsia",
+  "ghostwhite",
+  "gold",
+  "goldenrod",
+  "gray",
+  "green",
+  "indigo",
+  "ivory",
+  "khaki",
+  "lavender",
+  "lime",
+  "linen",
+  "magenta",
+  "maroon",
+  "moccasin",
+  "navy",
+  "olive",
+  "orange",
+  "orchid",
+  "peru",
+  "pink",
+  "plum",
+  "purple",
+  "red",
+  "salmon",
+  "sienna",
+  "silver",
+  "snow",
+  "tan",
+  "teal",
+  "thistle",
+  "tomato",
+  "turquoise",
+  "violet",
+  "white",
+  "yellow",
+];
 
 var recognition = new SpeechRecognition();
+
 if (SpeechGrammarList) {
-  // SpeechGrammarList is not currently available in Safari, and does not have any effect in any other browser.
-  // This code is provided as a demonstration of possible capability. You may choose not to use it.
   var speechRecognitionList = new SpeechGrammarList();
-  var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+  var grammar =
+    "#JSGF V1.0; grammar colors; public <color> = " + colors.join(" | ") + " ;";
   speechRecognitionList.addFromString(grammar, 1);
   recognition.grammars = speechRecognitionList;
 }
+
 recognition.continuous = false;
-recognition.lang = 'en-US';
+recognition.lang = "en-US";
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-var diagnostic = document.querySelector('.output');
-var bg = document.querySelector('html');
-var hints = document.querySelector('.hints');
+var diagnostic = document.querySelector(".output");
+var confidence = document.querySelector(".confidence-text");
+var debugResult = document.querySelector(".debug-result");
 
-var colorHTML= '';
-colors.forEach(function(v, i, a){
-  console.log(v, i);
-  colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
+var bg = document.querySelector("html");
+
+var hints = document.querySelector(".hints");
+
+var clickButton = document.querySelector(".click-me");
+
+var gridChipOfColors = document.querySelector(".chip-of-colors");
+
+var colorHTML = "";
+
+var classStringColorChip =
+  "capitalize duration-500 ease-out flex font-medium hover:ease-in hover:scale-105 hover:shadow-lg items-center justify-center py-2 rounded-md shadow-sm text-center transform transition-all w-full";
+
+var isDark = (colorName) => {
+  switch (colorName) {
+    case "black":
+    case "navy":
+    case "navy":
+    case "indigo":
+    case "maroon":
+    case "black":
+    case "purple":
+      return true;
+
+    default:
+      return false;
+  }
+};
+
+var textColorClass = (colorName) => {
+  return isDark(colorName) ? "text-white" : "text-black";
+};
+
+colors.forEach(function(colorName, index) {
+  colorHTML +=
+    `<span style="background-color:${colorName};" class="${classStringColorChip} ${textColorClass(
+      colorName
+    )}"> ` +
+    colorName +
+    " </span>";
 });
-hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try ' + colorHTML + '.';
 
-document.body.onclick = function() {
+hints.innerHTML =
+  "Tap then say a color to change the background color of the app. Try:" + ".";
+
+gridChipOfColors.innerHTML = colorHTML;
+
+clickButton.onclick = function() {
   recognition.start();
-  console.log('Ready to receive a color command.');
-}
+  console.log("Ready to receive a color command.");
+};
 
 recognition.onresult = function(event) {
-  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-  // It has a getter so it can be accessed like an array
-  // The first [0] returns the SpeechRecognitionResult at the last position.
-  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-  // These also have getters so they can be accessed like arrays.
-  // The second [0] returns the SpeechRecognitionAlternative at position 0.
-  // We then return the transcript property of the SpeechRecognitionAlternative object
   var color = event.results[0][0].transcript;
-  diagnostic.textContent = 'Result received: ' + color + '.';
+
+  diagnostic.innerHTML = `Result received: <strong class="text-indigo-200 capitalize"> ${color} </strong> .`;
+
   bg.style.backgroundColor = color;
-  console.log('Confidence: ' + event.results[0][0].confidence);
-}
+
+  confidence.innerHTML = `Confidence: ${"Confidence: " +
+    event.results[0][0].confidence}`;
+
+  console.log("Confidence: " + event.results[0][0].confidence, event.results);
+};
 
 recognition.onspeechend = function() {
   recognition.stop();
-}
+};
 
 recognition.onnomatch = function(event) {
   diagnostic.textContent = "I didn't recognise that color.";
-}
+};
 
 recognition.onerror = function(event) {
-  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
-}
+  diagnostic.textContent = "Error occurred in recognition: " + event.error;
+};
